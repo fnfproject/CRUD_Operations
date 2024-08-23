7d096437d7359f23fdc2dd77b213bf5ac528eb77
fnfproject <fnfproject2024@gmail.com>
2024-08-23 16:27:10 +0530

    Add files via upload

---
+++ Dtos/Question.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectApi.Dtos
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        [Required]
+        public string Subject { get; set; }
+        [Required]
+        public string Topic { get; set; }
+        [Required]
+        public string DifficultyLevel { get; set; }
+        [Required]
+        public string QuestionText { get; set; }
+        [Required]
+        public string OptionA { get; set; }
+        //public string OptionAImagePath { get; set; }
+        [Required]
+        public string OptionB { get; set; }
+        //public string OptionBImagePath { get; set; }
+        [Required]
+        public string OptionC { get; set; }
+        //public string OptionCImagePath { get; set; }
+        [Required]
+        public string OptionD { get; set; }
+        //public string OptionDImagePath { get; set; }
+        [Required]
+        public string CorrectAnswer { get; set; }
+        [Required]
+        public string Explaination { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Dtos/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectApi.Dtos
+{
+    public class Test
+    {
+        public int TestId { get; set; }
+        [Required]
+        public string TestName { get; set; }
+        public int TestMaxMarks { get; set; }
+        public int TestNoOfQuestions { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime ExpiryTime { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
